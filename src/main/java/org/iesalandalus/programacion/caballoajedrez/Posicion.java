e463d73af85651aabfa3135f0d6dc6c148d9772a
Jose <Jose@Jose-PC>
2018-11-21 15:07:47 +0100

    Test pasados y validacion de datos correctos en el método set

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -6,6 +6,49 @@ public class Posicion {
 	char columna;
 	
 	
+	//Get y Set
+	
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		
+		if (fila >= 1 || fila <= 8) {
+			
+			this.fila = fila;
+			
+		}else{ 
+			
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+		
+	}
+	public char getColumna() {
+		
+			return columna;	
+		}
+		
+		
+	
+	public void setColumna(char columna) {
+		if (columna >= 'a' && columna <='h' ){
+			
+		 this.columna = columna;
+	
+		}else{
+			
+		throw new IllegalArgumentException("ERROR: Columna  no válida.");
+		
+	          }
+		}
+		
+	
+	
+	//Constructor 
+	
+	public Posicion (int fila, char columna) {
+		
+	}
 	
 
 }
